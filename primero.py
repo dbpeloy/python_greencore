8b95a50306e735cc7d1c301cb0b63d8a4238010b
edwar.vallejos.e@gmail.com <edwar.vallejos.e@gmail.com>
2023-02-13 23:55:54 +0000

    Ejemplos

---
+++ primero.py
@@ -0,0 +1,28 @@
+# Integer
+a = 2
+print(a)
+
+# Integer
+b = 9223372036854775807
+print(b)
+
+# Floating point
+pi = 3.14
+print(pi)
+
+# String
+c = 'A'
+print(c)
+
+# String
+name = 'John Doe'
+print(name)
+
+# Boolean
+q = True
+print(q)
+
+# Empty value or null data type
+x = None
+print(x)
+
---
+++ segundo.py
@@ -0,0 +1,20 @@
+a = 2
+print(type(a))
+
+b = 9223372036854775807
+print(type(b))
+
+pi = 3.14
+print(type(pi))
+
+c = 'A'
+print(type(c))
+
+name = 'John Doe'
+print(type(name))
+
+q = True
+print(type(q))
+
+x = None
+print(type(x))
