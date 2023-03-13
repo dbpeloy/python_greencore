4dbd33cd2f7f97acbce11acf90bdced292f7c0b7
edwar.vallejos.e@gmail.com <edwar.vallejos.e@gmail.com>
2023-03-13 23:12:40 +0000

    Cambios ultimos

---
+++ clase13marzo/intento-tarea-edwar.py
@@ -0,0 +1,31 @@
+# Ciclo para contar del 1 al 100
+# Ejercicio de mate
+# Imprima los números del 1 al 100, agregando un caracter especial si el número es divisible entre 2, 3 o 5.
+
+# Ciclo para contar del 1 al 100
+
+# Intentando resolver
+ 
+# Pedir número inicial y final de la lista que se va a crear
+inicio = int(input("Ingrese el número inicial de la lista......: "))
+fin = int(input("Ingrese el número final de la lista......: "))
+
+# Pedir los tres números divisores
+divisor1 = int(input("Ingrese el primer número divisor: "))
+divisor2 = int(input("Ingrese el segundo número divisor: "))
+divisor3 = int(input("Ingrese el tercer número divisor: "))
+
+# Crear la lista de números
+numeros = list(range(inicio, fin+1))
+
+# Crear una nueva lista con los números divisibles por los tres divisores pero solo me saca los divisibles exactos
+# por ejemplo haciendo una lista de 0 a 100 me dice que los números divisibles por 2 3 y 5 son: [30, 60, 90]
+# aqui ==>  
+
+divisibles = [num for num in numeros if num % divisor1 == 0 and num % divisor2 == 0 and num % divisor3 == 0]
+
+# Imprimir los números divisibles encontrados
+print("Los números divisibles por", divisor1, divisor2, "y", divisor3, "son:", divisibles)
+
+# Falta 0 para Terminar
+
--- clase13marzo/tarea-edwar.py
+++ clase13marzo/tarea-edwar.py
@@ -4,6 +4,7 @@
 
 # Ciclo para contar del 1 al 100
 
+## **** Preguntar como se comenta desde un inicio a un final !!??
 
 # Funcion para imprimir listas de divisibles en columnas
 
@@ -27,13 +28,14 @@
 
 #divisibles(lista, None)
 
-# Intentando 
 
-# Pedir al usuario el número inicial y final de la lista
-inicio = int(input("Ingrese el número inicial de la lista: "))
-fin = int(input("Ingrese el número final de la lista: "))
+# Intentando resolver
+ 
+# Pedir número inicial y final de la lista que se va a crear
+inicio = int(input("Ingrese el número inicial de la lista......: "))
+fin = int(input("Ingrese el número final de la lista......: "))
 
-# Pedir al usuario los tres números divisores
+# Pedir los tres números divisores
 divisor1 = int(input("Ingrese el primer número divisor: "))
 divisor2 = int(input("Ingrese el segundo número divisor: "))
 divisor3 = int(input("Ingrese el tercer número divisor: "))
@@ -41,9 +43,24 @@ divisor3 = int(input("Ingrese el tercer número divisor: "))
 # Crear la lista de números
 numeros = list(range(inicio, fin+1))
 
-# Crear una nueva lista con los números divisibles por los tres divisores
-divisibles = [num for num in numeros if num % divisor1 == 0 and num % divisor2 == 0 and num % divisor3 == 0]
+# Crear una nueva lista con los números divisibles por los tres divisores pero solo me saca los divisibles exactos
+# por ejemplo haciendo una lista de 0 a 100 me dice que los números divisibles por 2 3 y 5 son: [30, 60, 90]
+# aqui ==>  divisibles = [num for num in numeros if num % divisor1 == 0 and num % divisor2 == 0 and num % divisor3 == 0]
+
+# Intento con el IF del ejemplo de la clase
+
+for num in numeros = (list(range(inicio, fin+1))) : 
+    # Verificando si el número es divisible por 2, 3 y 5
+    if num % 2 == 0 or num % 3 == 0 or num % 5 == 0:
+      print('{:>6}'.format(num),"*", end="")
+    else:
+        print('{:>6}'.format(num), end="  ")
+    if num % 10 == 0 :
+        print("")
+num=1
 
 # Imprimir los números divisibles encontrados
 print("Los números divisibles por", divisor1, divisor2, "y", divisor3, "son:", divisibles)
+
+# Falta 0 para Terminar
 
